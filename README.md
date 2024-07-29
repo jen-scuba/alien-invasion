115cfaab03be530bd308f44d726c66e98c7b9578
Jen Kuenning <jkue@loc.gov>
2024-07-29 16:46:58 -0400

    moving left and right

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ https://ehmatthes.github.io/pcc_3e/
 
 Project: Alien Invasion
 Ch 12-13-14
+
+https://opengameart.org
+https://htmlcolorcodes.com/color-picker/
+https://cloudconvert.com/png-to-bmp
+
+
+helper methods begin with an underscore ex. _check_events() and only affect the code within the enclosing function ....?
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,9 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+# from monk import Monk
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,20 +18,41 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+        # self.ship = Monk(self)
+
     def run_game(self):
         """Start the main loop of the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit
+            elif event.type == pygame.KEYDOWN:
+                # Move the ship.
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+            
+
+    def _update_screen(self):
+        """Update imagaes on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
