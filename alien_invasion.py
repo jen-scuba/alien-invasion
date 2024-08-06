cff0ee28df2a9683e24973b5c21a4ef9e0899b8e
Jen Kuenning <jkue@loc.gov>
2024-08-06 16:20:29 -0400

    firing bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 # from monk import Monk
-
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -20,12 +20,22 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         # self.ship = Monk(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         """Start the main loop of the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            # print(len(self.bullets))
+
             self._update_screen()
             self.clock.tick(60)
 
@@ -48,6 +58,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
 
     def _check_keyup_events(self, event):
@@ -57,10 +69,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
             
+    def _fire_bullet(self):
+        """Create a new bullet and add to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update imagaes on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
